e3d9959afc35ff8fe7ad55021a87e71f65b7f089
MURUGAN96 <1murugan23@gmail.com>
2022-08-06 18:03:46 +0530

    2 files modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,13 +1,77 @@
 import * as React from 'react';
-import Home from './home';
+import {
+  useState,
+  useEffect,
+  useRef,
+  useContext,
+  createContext,
+  useReducer,
+} from 'react';
 import './style.css';
 
+export const BirdContext = createContext({
+  newBird: { bird: 'parrot' },
+  setBird: () => null,
+});
+
+export const BirdProvider = ({ children }) => {
+  const [newBird, setBird] = useState({ bird: 'parrot' });
+  const value = { newBird, setBird };
+  return <BirdContext.Provider value={value}>{children}</BirdContext.Provider>;
+};
+
+const initialState = {
+  count: 0,
+};
+const teleReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case 'DECREMENT':
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
+
 export default function App() {
+  const [animal, setAnimal] = useState(0);
+
+  const myBtn = useRef(0);
+  const [state, dispatch] = useReducer(teleReducer, initialState);
+
+  useEffect(() => {
+    myBtn.current += 1;
+    setTimeout(() => {
+      console.log('created');
+      document.getElementById('textValue').innerHTML = 'LION';
+    }, 1000);
+  });
+
+  const BirdProvider = useContext(BirdContext);
+  console.log(BirdProvider.newBird);
+
+  const getCall = () => {
+    dispatch({ type: 'INCREMENT' })
+    console.log(`63: ` +state.count);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Home />
+      <h1>Hello !</h1>
+      <h1>React Hooks</h1>
+      <button onClick={() => setAnimal(animal + 1)}>Add Animal Lion</button>
+      {animal == 0 ? null : <h1>{animal}</h1>}
+      <h3 id="textValue"></h3>
+      <h2>{myBtn.current}</h2>
+      <button onClick={getCall}>Invoke Reducer Hook</button>
     </div>
   );
 }
--- home.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, createContext, useReducer } from 'react';
-
-const Home = () => {
-  <>
-    <h1>This is from home</h1>
-  </>;
-};
-
-export default Home;
